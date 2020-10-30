2a1b8e75a5b0f240c14a155017b62c4f727dfeb1
mpatel18 <meekitpatel@gmail.com>
2020-10-30 17:33:17 -0400

    added dates to posts, post ordered by last added

--- blog_site/models.py
+++ blog_site/models.py
@@ -6,13 +6,16 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=255, help_text="Enter a Title")
     title_tag = models.CharField(max_length=255, help_text='Enter a Tag for your Post')
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+
+    class Meta:
+       ordering = ['-date_added']
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
     
     def get_absolute_url(self):
         return reverse('post', args=(str(self.pk)))
-    
-
