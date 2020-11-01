700eaf0a8c557a61fb37ddd5d8b90ec9ed0058bb
mpatel18 <meekitpatel@gmail.com>
2020-11-01 15:36:15 -0500

    added styling to pages

--- blog_site/forms.py
+++ blog_site/forms.py
@@ -9,15 +9,26 @@ class Form(forms.ModelForm):
         fields = ('title', 'title_tag', 'body')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'length'}),
-            'title_tag': forms.TextInput(attrs={'class': 'length'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta: 
         model = User 
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
