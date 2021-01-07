034742056e711aadc9a465433ca75b631f7e4434
mpatel18 <meekitpatel@gmail.com>
2021-01-07 16:48:20 -0500

    added allowed hosts

--- weblog/settings.py
+++ weblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3g$g1gjfm(+!x!a!1u!m20kymbc+wyt+1##95_+ibl5m91l+b9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nameless-earth-86565.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
